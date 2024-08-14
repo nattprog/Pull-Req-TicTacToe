04aa1fe2478febc7b9b6ea60b314cf4109649b07
JiaCheng <chengjialew123@outlook.com>
2024-08-14 15:47:59 +0800

    Im the winner

---
+++ testtest.py
@@ -0,0 +1,38 @@
+from TTTLib import *
+BoardInitiate()
+# .......................................
+#           x=-1  x=0   x=1
+#      y=1      |     |     
+#          -----------------
+#      y=0      |     |     
+#          -----------------
+#      y=-1     |     |     
+# .......................................
+# Enter your move like this:
+
+# Noughts(x-value, y-value)
+
+# Crosses(x-value, y-value)
+
+# eg:
+# Noughts(-1, 0)
+# Crosses(0, 1)
+# .......................................
+# Add subsequent moves below previous moves.
+# ............ Enter Code Below ............
+
+# Noughts: [LJC]
+# Crosses: [CYH]
+
+Noughts(0,0)
+Crosses(-1,1)
+Noughts(1,1)
+Crosses(-1,-1)
+Noughts(0,1)
+
+
+
+
+
+# ............ End Of Code ............
+t.screen.mainloop()
